79b1cc78005450ec0dcbe8c64b151d06e50b04bd
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-09-15 02:57:27 +0600

    updated youtube section

--- src/components/Youtube/Video.js
+++ src/components/Youtube/Video.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Video = ({video}) => {
+const Video = ({ video }) => {
     return (
-        <div class="aspect-w-16 aspect-h-9 flex items-center justify-center">
-            <iframe width="560" height="315" src={video.source} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <div className="p-2 aspect-w-16 aspect-h-9 flex items-center justify-center">
+            <iframe
+                width="560"
+                height="315"
+                src={video.source}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+                className="rounded-lg shadow-lg hover:shadow-xl transition duration-300"
+            ></iframe>
         </div>
     );
 };
