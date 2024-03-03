572bb2ded27b37a85ae199a5781fce5d12dc108a
Mehedi Ahsan <mdmehedicse01@gmail.com>
2024-03-03 22:07:04 +0600

    added swiper

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
-import Project from "./Project";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import SampleNextArrow from "./SampleNextArrow";
-import SamplePrevArrow from "./SamplePrevArrow";
-import "./custom-carousel.css"; // Import your custom CSS file
-import { Link } from "react-router-dom";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+
+import Project from './Project';
+import './custom-carousel.css'
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
@@ -17,51 +21,12 @@ const Projects = () => {
       .then((data) => setProjects(data));
   }, []);
 
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 2000,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-    initialSlide: 0,
-    autoplaySpeed: 2000,
-    autoplay: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 0,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
   return (
     <div
       data-aos="fade-up"
       data-aos-duration="500"
       name="projects"
-      className="container mx-auto my-28"
+      className="container mx-auto my-28 relative"
     >
       {/* <h1 className='text-3xl font-bold text-center my-20 text-slate-200'>My Latest<span className='text-teal-500'> Projects</span></h1> */}
       <div class="container mb-16">
@@ -75,35 +40,43 @@ const Projects = () => {
         </div>
       </div>
 
-      <div className="px-8 md:px-44">
-        <Slider {...settings}>
-          {projects.map((project) => (
-            <Project key={project.id} project={project}></Project>
-          ))}
-        </Slider>
-        <button className="pt-10">
-          <Link
-            to="/projects"
-            class="flex items-center border border-primary text-white gap-1 px-2 py-1 cursor-pointer tracking-widest rounded bg-blue-500 duration-300 hover:gap-2 hover:translate-x-3 shadow-md shadow-primary"
-          >
-            View All
-            <svg
-              class="w-5 h-5"
-              stroke="currentColor"
-              stroke-width="1.5"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-                stroke-linejoin="round"
-                stroke-linecap="round"
-              ></path>
-            </svg>
-          </Link>
-        </button>
+      <div className="border-b border-[#012838] mb-16">
+        <button className="bg-primary p-2 text-black">Most Views</button>
       </div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        loop={true}
+        // navigation={true}
+        navigation={{
+          prevEl: ".swiper-button-prev",
+          nextEl: ".swiper-button-next",
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+        }}
+        modules={[Navigation]}
+        className="mySwiper custom-swiper"
+      >
+        {projects.map((project, i) => (
+          <SwiperSlide key={i}>
+            <Project project={project}></Project>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
     </div>
   );
 };
